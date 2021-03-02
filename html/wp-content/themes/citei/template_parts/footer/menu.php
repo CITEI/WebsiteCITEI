b5feb7859233c804e222cfc30e847a2262dafbac
nickolas da rocha machado <nickolas123full@gmail.com>
2021-03-02 20:15:55 -0300

    added home to footer menu

--- html/wp-content/themes/citei/template_parts/footer/menu.php
+++ html/wp-content/themes/citei/template_parts/footer/menu.php
@@ -7,6 +7,9 @@ Author: Nickolas da Rocha Machado & Natalia Zambe
 ?>
 <h5 class="text-uppercase">Mapa do site</h5>
 <ul class="list-unstyled m-0 my-2">
+    <li class="mt-2"><a class="text-reset" href="<?php echo get_home_url() ?>">
+        Início
+    </a></li>
 <?php
     $menu = wp_get_menu_array(get_nav_menu_locations()['primary']);
     foreach ($menu as $item)
